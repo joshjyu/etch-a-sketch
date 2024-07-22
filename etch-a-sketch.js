bc2f19b15431bcf7bd7557f922c71668969a8816
joshjyu <jjyu98@gmail.com>
2024-07-22 10:56:59 -0400

    Add mouse hover effects to grid squares

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -5,3 +5,13 @@ for (let i = 0; i < 256; i++) {
   div.classList.add("gridDiv");
   container.appendChild(div);
 }
+
+const gridDiv = document.querySelectorAll(".gridDiv");
+for (let i = 0; i < gridDiv.length; i++) {
+  gridDiv[i].addEventListener("mouseenter", () => {
+    gridDiv[i].setAttribute("style", "background: #c0d2f3;")
+  });
+  gridDiv[i].addEventListener("mouseleave", () => {
+    gridDiv[i].setAttribute("style", "background: #ffffff;")
+  });
+}
