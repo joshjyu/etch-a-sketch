86e8a684a7f9f2e29778006bc61a3dd2b4129274
joshjyu <jjyu98@gmail.com>
2024-07-22 13:52:15 -0400

    Add button to set grid density
    Since the grid is designed with flexbox, adding the grid density button required
    rewriting some of the JS so that the flex basis dynamically updates. Also rewrote
    the mouseover and mouseout event listeners to make the dynamic flex basis work.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,17 +1,50 @@
 const container = document.querySelector(".container");
+let gridLength = 16;
 
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div");
-  div.classList.add("gridDiv");
-  container.appendChild(div);
+function drawGrid() {
+  for (let i = 0; i < Math.pow(gridLength, 2); i++) {
+    const div = document.createElement("div");
+    div.classList.add("gridDiv");
+    div.style.cssText = `flex: 0 1 ${100 / gridLength}%; background-color: white;`;
+    container.appendChild(div);
+  }
 }
 
-const gridDiv = document.querySelectorAll(".gridDiv");
-for (let i = 0; i < gridDiv.length; i++) {
-  gridDiv[i].addEventListener("mouseenter", () => {
-    gridDiv[i].setAttribute("style", "background: #c0d2f3;")
-  });
-  gridDiv[i].addEventListener("mouseleave", () => {
-    gridDiv[i].setAttribute("style", "background: #ffffff;")
-  });
+drawGrid();
+
+function divHoverInHandler(event) {
+  const el = event.target;
+  if (el.classList.contains("gridDiv")) {
+    el.style.backgroundColor = "grey";
+  }
+}
+
+function divHoverOutHandler(event) {
+  const el = event.target;
+  if (el.classList.contains("gridDiv")) {
+    el.style.backgroundColor = "white";
+  }
+}
+
+container.addEventListener("mouseover", divHoverInHandler);
+container.addEventListener("mouseout", divHoverOutHandler);
+
+const gridDensityPrompt = document.querySelector("#prompt");
+gridDensityPrompt.addEventListener("click", setGridDensity);
+
+function setGridDensity() {
+  gridLength = prompt("Enter a value between 16 and 100 to set as the number of squares per side of the grid.").trim();
+
+  while (!(gridLength >= 16 && gridLength <= 100)) {
+    gridLength = prompt(
+      "That is not a number between 16 and 100. Please enter a grid side length between 16 and 100.",
+    ).trim();
+  }
+
+  const containerNode = document.querySelector(".container");
+  while (containerNode.firstChild) {
+    containerNode.removeChild(containerNode.lastChild);
+  }
+
+  drawGrid();
 }
