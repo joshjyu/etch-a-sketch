026c9f59979325d7f45e8624f3600d5915b1abb1
joshjyu <jjyu98@gmail.com>
2024-07-22 14:38:18 -0400

    Modify mouse trail colors to random RGB values

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -14,8 +14,11 @@ drawGrid();
 
 function divHoverHandler(event) {
   const el = event.target;
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
   if (el.classList.contains("gridDiv")) {
-    el.style.backgroundColor = "grey";
+    el.style.backgroundColor = `rgb(${red}, ${blue}, ${green})`;
   }
 }
 
@@ -25,7 +28,9 @@ const gridDensityPrompt = document.querySelector("#prompt");
 gridDensityPrompt.addEventListener("click", setGridDensity);
 
 function setGridDensity() {
-  gridLength = prompt("Enter a value between 16 and 100 to set as the number of squares per side of the grid.").trim();
+  gridLength = prompt(
+    "Enter a value between 16 and 100 to set as the number of squares per side of the grid.",
+  ).trim();
 
   while (!(gridLength >= 16 && gridLength <= 100)) {
     gridLength = prompt(
